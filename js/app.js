964ee79c7db40a46f49ead993fe473f9f9ae0c82
vatscoder <70788329+vatscoder@users.noreply.github.com>
2020-10-01 09:54:23 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,4 +14,4 @@ setInterval(() => {
   hr.style.transform = `rotate(${hrotation}deg)`;
   mn.style.transform = `rotate(${mrotation}deg)`;
   sc.style.transform = `rotate(${srotation}deg)`;
-})
+}, 1000);
